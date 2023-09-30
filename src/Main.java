fb7011c523caabaf3686d939b62eb8103364db47
jrodriguezdiazz <jrodriguezdiazz@outlook.com>
2023-09-30 18:07:31 -0400

    chore: refactor, use Main file

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,93 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
-public class Main {
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Arrays;
+
+public class Main extends JPanel {
+
+    private JTextField targetNumberTextField;
+    private JTextField arrayToOrderTextField;
+    private JButton startButton;
+    private JButton resetButton;
+    private JTextField sequentialSearchTextField;
+    private JTextField binarySearchTextField;
+    private JTextField bubbleSortTextField;
+    private JTextField insertionSortTextField;
+    private JTextField quickSortTextField;
+
+    public Main() {
+        targetNumberTextField = new JTextField(10);
+        arrayToOrderTextField = new JTextField(20);
+        startButton = new JButton("Start");
+        resetButton = new JButton("Reset");
+        sequentialSearchTextField = new JTextField(30);
+        binarySearchTextField = new JTextField(30);
+        bubbleSortTextField = new JTextField(30);
+        insertionSortTextField = new JTextField(30);
+        quickSortTextField = new JTextField(30);
+
+        startButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                performSearchAndSort();
+            }
+        });
+
+        resetButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                resetFields();
+            }
+        });
+
+        // Agrega componentes al panel
+        add(new JLabel("Array to Order:"));
+        add(arrayToOrderTextField);
+        add(new JLabel("Target Number:"));
+        add(targetNumberTextField);
+        add(startButton);
+        add(resetButton);
+        add(new JLabel("Results:"));
+        add(sequentialSearchTextField);
+        add(binarySearchTextField);
+        add(bubbleSortTextField);
+        add(insertionSortTextField);
+        add(quickSortTextField);
+    }
+
+    private void performSearchAndSort() {
+        // Tu lógica para buscar y ordenar aquí...
+
+        // Por ejemplo, aquí establecemos un mensaje de demostración:
+        sequentialSearchTextField.setText("Sequential Search Result");
+        binarySearchTextField.setText("Binary Search Result");
+        bubbleSortTextField.setText("Bubble Sort Result");
+        insertionSortTextField.setText("Insertion Sort Result");
+        quickSortTextField.setText("Quick Sort Result");
+    }
+
+    private void resetFields() {
+        targetNumberTextField.setText("");
+        arrayToOrderTextField.setText("");
+        sequentialSearchTextField.setText("");
+        binarySearchTextField.setText("");
+        bubbleSortTextField.setText("");
+        insertionSortTextField.setText("");
+        quickSortTextField.setText("");
+    }
+
     public static void main(String[] args) {
-        // Press Opt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                JFrame frame = new JFrame("Search and Sort App");
+                frame.setSize(800, 600);
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        // Press Ctrl+R or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+                frame.getContentPane().add(new Main());
 
-            // Press Ctrl+D to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Cmd+F8.
-            System.out.println("i = " + i);
-        }
+                frame.setVisible(true);
+            }
+        });
     }
 }
