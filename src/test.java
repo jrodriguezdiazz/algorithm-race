06caac61d83a2d7ce2cd1e2506369d91ade349a3
jrodriguezdiazz <jrodriguezdiazz@outlook.com>
2023-09-30 16:26:14 -0400

    chore: add layout

---
+++ src/test.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class test {
+    private JTextField targetNumberTextField;
+    private JTextField arrayToOrderTextField;
+    private JButton startButton;
+    private JButton resetButton;
+    private JTextField sequentialSearchTextField;
+    private JTextField binarySearchTextField;
+    private JTextField bubbleSortTextField;
+    private JTextField insertionSortTextField;
+    private JTextField quickSortTextField;
+}
